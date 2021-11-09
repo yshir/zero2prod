e08f6d4fc217a8b4e20b9e79a8fc3a84f8d15071
yshir <yshir0020@gmail.com>
2021-11-10 00:52:44 +0900

    feat: impl newsletters

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -154,7 +154,7 @@ pub async fn send_confirmation_email(
     );
 
     email_client
-        .send_email(new_subscriber.email, "Welcome!", html_body, plain_body)
+        .send_email(&new_subscriber.email, "Welcome!", html_body, plain_body)
         .await
 }
 
@@ -208,7 +208,7 @@ impl std::error::Error for StoreTokenError {
 
 impl ResponseError for StoreTokenError {}
 
-fn error_chain_fmt(
+pub fn error_chain_fmt(
     e: &impl std::error::Error,
     f: &mut std::fmt::Formatter<'_>,
 ) -> std::fmt::Result {
